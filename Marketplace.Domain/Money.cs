b334548be14524ca00d94cd6d22bb44e82d9d43d
panospd <anastapd@gmail.com>
2020-10-06 10:23:32 +0100

    Implemented price and money value objects, validation and currencylookup domain service

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -5,31 +5,76 @@ namespace Marketplace.Domain
 {
     public class Money : Value<Money>
     {
-        public static Money FromDecimal(decimal amount) => 
-            new Money(amount);
+        private const string DefaultCurrency = "EUR";
 
-        public static Money FromString(string amount) => 
-            new Money(decimal.Parse(amount));
+        public static Money FromDecimal(decimal amount, string currency, ICurrencyLookup currencyLookup) => 
+            new Money(amount, currency, currencyLookup);
 
-        public Money(decimal amount)
+        public static Money FromString(string amount, string currency, ICurrencyLookup currencyLookup) => 
+            new Money(decimal.Parse(amount), currency, currencyLookup);
+
+        public Money(decimal amount, string currencyCode, ICurrencyLookup currencyLookup)
         {
-            if(decimal.Round(amount, 2) != amount)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Amount cannot have more that two decimals");
+            if(string.IsNullOrEmpty(currencyCode))
+                throw new ArgumentNullException(nameof(currencyCode), "Currency code must be specified");
+
+            var currency = currencyLookup.FindCurrency(currencyCode);
+
+            if(!currency.InUse)
+                throw new ArgumentException($"Currency {currencyCode} is not valid");
+
+            if (decimal.Round(amount, currency.DecimalPlaces) != amount)
+                throw new ArgumentOutOfRangeException(nameof(amount), $"Amount in {currencyCode} cannot have more that {currency.DecimalPlaces} decimals");
+
+            
 
             Amount = amount;
+            Currency = currency;
+        }
+
+        private Money(decimal amount, CurrencyDetails currency)
+        {
+            Amount = amount;
+            Currency = currency;
         }
 
         public decimal Amount { get; }
+        public CurrencyDetails Currency{ get; }
 
-        public Money Add(Money summand) => new Money(Amount + summand.Amount);
+        public Money Add(Money summand)
+        {
+            if (Currency != summand.Currency)
+                throw new CurrencyMisMatchException("Cannot sum amounts with different currencies");
 
-        public  Money Subtract(Money subtrahend) => 
-            new Money(Amount - subtrahend.Amount);
+            return new Money(Amount + summand.Amount, Currency);
+        }
+
+        public Money Subtract(Money subtrahend)
+        {
+            if (Currency != subtrahend.Currency)
+                throw new CurrencyMisMatchException("Cannot subtract amounts with different currencies");
+
+            return new Money(Amount - subtrahend.Amount, Currency);
+        }
+           
 
         public static Money operator +(Money summand1, Money summand2) => 
             summand1.Add(summand2);
 
         public static Money operator -(Money subtrahend1, Money subtrahend2) =>
             subtrahend1.Subtract(subtrahend2);
+
+        public override string ToString()
+        {
+            return $"{Currency.CurrencyCode} {Amount}";
+        }
+    }
+
+    public class CurrencyMisMatchException : Exception
+    {
+        public CurrencyMisMatchException(string message)
+            : base(message)
+        {
+        }
     }
 }
