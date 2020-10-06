bb55fc033f8ef1b5d6f58c8253d3bd288d1e4ec8
panospd <anastapd@gmail.com>
2020-10-06 16:27:33 +0100

    Created the basis of classifiedAd command handling

--- Marketplace/Api/ClassifiedAdsCommandsApi.cs
+++ Marketplace/Api/ClassifiedAdsCommandsApi.cs
@@ -6,12 +6,26 @@ namespace Marketplace.Api
     [Route("/ad")]
     public class ClassifiedAdsCommandsApi : Controller
     {
+        private readonly ClassifiedAdsApplicationService _applicationService;
+
+        public ClassifiedAdsCommandsApi(ClassifiedAdsApplicationService _applicationService)
+        {
+            this._applicationService = _applicationService;
+        }
+
         [HttpPost]
         public async Task<IActionResult> Post(Contracts.ClassifiedAds.V1.Create request)
         {
-            // handle request
-
+            _applicationService.Handle(request);
             return Ok();
+        }
+    }
+
+    public class ClassifiedAdsApplicationService
+    {
+        public void Handle(Contracts.ClassifiedAds.V1.Create command)
+        {
+            // we need to create a bew Classified Ad here
         }
     }
 }
