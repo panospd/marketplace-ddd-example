1205d9a665327a258a09df65c63b5d34e1287046
panospd <panos.anastasiadis@cloudcommercegroup.com>
2020-12-26 15:59:25 +0000

    Added read and query models

---
+++ Marketplace/ClassifiedAd/ClassifiedAdsQueryApi.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.ClassifiedAd
+{
+    public class ClassifiedAdsQueryApi : Controller
+    {
+        [HttpGet]
+        [Route("list")]
+        public Task<IActionResult> Get(QueryModels.GetPublishedClassifiedAds request)
+        {
+        }
+        
+        [HttpGet]
+        [Route("myads")]
+        public Task<IActionResult> Get(QueryModels.GetOwnersClassifiedAd request)
+        {
+        }
+
+        [HttpGet]
+        [ProducesResponseType((int) HttpStatusCode.OK)]
+        [ProducesResponseType((int) HttpStatusCode.NotFound)]
+        public Task<IActionResult> Get(QueryModels.GetPublicClassifiedAd request)
+        {
+        }
+    }
+}
